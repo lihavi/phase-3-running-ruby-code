7a7c9e5c8e60108c6fa9808e4eb39b5d925fc05c
vivianne <viviannjoroge91@gmail.com>
2023-02-04 18:46:02 +0300

    commit tree 5d82a52848308de438da98c6e7976bdbaa079576

--- app.rb
+++ app.rb
@@ -4,9 +4,13 @@
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
-# puts "Hello World!"
+ puts "Hello World!"
+
 # print "Pass this test, please."
-# p [1,2,3]
+print "Pass this test, please."
+
+p [1,2,3]
+
 
 # def add
     
